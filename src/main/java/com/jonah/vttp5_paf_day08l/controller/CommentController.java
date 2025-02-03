49985bc0e18232cb1e6a062ad93af254eec0247b
jonahng <nyhj1998@gmail.com>
2025-02-03 14:20:09 +0800

    handle multiple default request parameters

--- src/main/java/com/jonah/vttp5_paf_day08l/controller/CommentController.java
+++ src/main/java/com/jonah/vttp5_paf_day08l/controller/CommentController.java
@@ -1,10 +1,12 @@
 package com.jonah.vttp5_paf_day08l.controller;
 
+import java.nio.channels.MulticastChannel;
 import java.util.List;
 
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +23,13 @@ public class CommentController {
 
 
     @GetMapping("/{user}")
-    public ResponseEntity<List<Document>> getUserComments(@PathVariable("user") String user, @RequestParam(defaultValue = "10") String limit){
+    public ResponseEntity<List<Document>> getUserComments(@PathVariable("user") String user, @RequestParam MultiValueMap<String,String> params){
+        //example of how to deal with multiple request params and set a default value for each param.   
+        String limit = params.getFirst("limit");
+
+        if(limit == null){
+            limit = "1";
+        }
         List<Document> result = commentService.getUserComments(user, Long.parseLong(limit));
         System.out.println("THE RESULT FOR USER COMMENTS IS:" + result.toString());
 
